7c49b88355e6b3ee8203bfbec0bd8a61199c475b
Zagg2732 <67727906+Zagg2732@users.noreply.github.com>
2022-02-23 18:38:09 +0900

    크롤링 정보 excel 저장

--- wanted.py
+++ wanted.py
@@ -1,10 +1,12 @@
 # Wanted 사이트를 크롤링하여 개발관련 인사이트를 제공하는 Project
 
+import os
 import requests
 import json
+import datetime
+from openpyxl import Workbook
 
-
-company_info_array = [] # 크롤링 회사 정보가 담길 배열
+company_info_array = []  # 크롤링 회사 정보가 담길 배열
 
 
 def init():
@@ -18,11 +20,12 @@ def init():
 
         if company_info is not None:
             company_info_array.append(company_info)
-
-    print(company_info_array)
+    make_excel(company_info_array)
 
 
 # function #
+
+# url 크롤링하여 얻은 회사정보
 def get_info(url, i):
     response = requests.get(url)
     html = response.text
@@ -38,18 +41,40 @@ def get_info(url, i):
     if category != 'IT':  # 카테고리가 IT인 회
         return None
     else:
+        company_info = {}
         wanted_json = json.loads(info_json)
         wanted_json = wanted_json['jobDetail']['head'][str(i)]
-        location = wanted_json['location']            # 지역
-        company_name = wanted_json['company_name']    # 회사이름
-        # main_tasks = wanted_json['main_tasks']      # 주요업무
-        # requirements = wanted_json['requirements']  # 자격요건
 
-    return [location, company_name]
+        company_info['회사이름'] = wanted_json['company_name']
+        company_info['지역'] = wanted_json['location']
+        company_info['주요업무'] = wanted_json['main_tasks']
+        company_info['자격요건'] = wanted_json['requirements']
+
+    return company_info
 
 
-def get_lang_by_jd(jd):  # 사용언어들 추출하기
-    print('job details = {}'.format(jd.replace("\\n", "\n")))
+# 회사정보를 통한 엑셀 파일 생성
+def make_excel(company_info):
+    write_wb = Workbook()  # openpyxl
+    write_ws = write_wb.active
+
+    # 컬럼 생성
+    write_ws.append(list(company_info[0].keys()))
+
+    # 크롤링 내용 삽입
+    for company in company_info:
+        excel_insert = []
+        for value in company.values():
+            excel_insert.append(value)
+        write_ws.append(excel_insert)
+
+    # 엑셀 저장
+    now = datetime.datetime.now()
+    now_date = now.strftime('%Y%m%d_%H%M%S')
+    path = os.path.abspath(__file__)[:-10] + '/excel/' # 저장경로는 프로젝트 폴더내의 excel 폴더
+    excel_title = path + 'wanted' + now_date + '.xlsx'
+
+    write_wb.save(excel_title)
 
 
 # init #
